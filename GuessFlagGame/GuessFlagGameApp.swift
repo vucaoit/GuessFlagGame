a5651196fcc2612b318790ed075591185392763f
vucaoit <86138777+vucaoit@users.noreply.github.com>
2023-04-04 13:54:08 +0700

    Initial Commit

---
+++ GuessFlagGame/GuessFlagGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagGameApp.swift
+//  GuessFlagGame
+//
+//  Created by vdcao on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
